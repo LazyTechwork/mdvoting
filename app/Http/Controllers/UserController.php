00c53fed8235adb56e5906b4f5bd5f1f4a4e2128
LazyTechwork <lazytechwork@gmail.com>
2020-03-29 18:19:50 +0300

    Registration logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\MessageBag;
@@ -42,6 +43,25 @@ class UserController extends Controller
 
     public function register(Request $request)
     {
+        $validator = validator($request->all(), [
+            'username' => ['required', 'string', 'exists:users,username'],
+            'password' => ['required', 'string', 'min:4', 'confirmed']
+        ], [
+            'username.required' => 'Введите имя пользователя',
+            'username.exists' => 'Логин или пароль введены неверно',
+            'password.required' => 'Вы забыли ввести свой пароль',
+            'password.confirmed' => 'Пароль должен быть подтверждён',
+            'password.min' => 'Минимальная длина пароля - 4'
+        ]);
 
+        if ($validator->fails())
+            return redirect()->back()->withErrors($validator)->withInput($request->all());
+
+        $credentials = $request->only('username', 'password');
+        User::create($credentials);
+        if (Auth::attempt($credentials))
+            return redirect(RouteServiceProvider::HOME);
+        else
+            return redirect(RouteServiceProvider::HOME);
     }
 }
