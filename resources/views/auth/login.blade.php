e27d4a3245756330c604aaa452bea5457bdd3e97
LazyTechwork <lazytechwork@gmail.com>
2020-03-29 17:12:28 +0300

    Vue scaffolding

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Вход в систему')
+
+@section('content')
+    
+@stop
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru_RU">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Система голосования MDVoting')</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
