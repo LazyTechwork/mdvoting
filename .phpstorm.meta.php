e33f0d6cc4aa3cf90f1465c5534bcd4d703b8d7b
LazyTechwork <lazytechwork@gmail.com>
2020-03-30 04:00:31 +0300

    Added PHP Spreadsheet parser + updated composer.json

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-03-30 00:14:16.
+    * Generated on 2020-03-30 00:59:05.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
